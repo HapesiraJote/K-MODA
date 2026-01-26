92f316d8bff8eaab6665ba19435349e7f0b2b76e
HapesiraJote <shkodranbeni@gmail.com>
2026-01-26 03:07:36 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const root = document.getElementById("root");
+
+root.innerHTML = `
+  <div style="min-height:100vh; display:flex; align-items:center; justify-content:center;">
+    <h1 style="font-size:3rem; font-weight:800;">
+      K-MODA po funksionon 🚀
+    </h1>
+  </div>
+`;
+
+console.log("JS u ngarkua OK ✅");
